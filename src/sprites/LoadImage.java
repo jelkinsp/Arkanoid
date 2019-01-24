f98ab31f3e93130838db63f915117e0ee7dc1c0f
Jose Luis Luengo <jlluengoramos@gmail.com>
2019-01-24 16:44:09 +0100

    Terminada la clase para cargas las imagenes

--- src/sprites/LoadImage.java
+++ src/sprites/LoadImage.java
@@ -8,7 +8,6 @@ import java.io.IOException;
  * Esta clase carga todas las imagenes y los tamaños de los sprites
  * Se pueden obtener mediante los GETTER.
  *
- *
  * @author : Jose Luis Luengo Ramos
  */
 public class LoadImage {
@@ -34,14 +33,14 @@ public class LoadImage {
     //Nave sin sombra.
     private final static int HEIGHT_BAR_SHIP = 32;
     private final static int WIDTH_BAR_SHIP = 8;
-
+    //Distacia entre las nave dentro de la imagen principal
     private final static int SIZE_BETWEEN_BAR_SHIP = 5;
 
 
-
-    private BufferedImage spritesSubBuffer;
+    private BufferedImage spritesBuffer;
     private BufferedImage ballSubBuffer;
-    private BufferedImage backgroundSubBuffer;
+    private BufferedImage liveUpSubBuffer;
+    private BufferedImage backgroundBuffer;
     private BufferedImage[] barShipSubBuffer;
     private BufferedImage[] barShipDestroidSubBuffer;
     private BufferedImage[] blockSubBuffer;
@@ -54,50 +53,93 @@ public class LoadImage {
         initializeShipBuffer();
         initializeShipDestroidBuffer();
         initializeBlockBuffer();
+        initializeLiveUpBuffer();
     }
 
+
     /**
-     * Inicializa el Sprite principal. Lee la imagen y la paso al spritesSubBuffer.
+     * Inicializa el Sprite principal. Lee la imagen y la paso al spritesBuffer.
      * Obtenemos los bloques, la bola de energia, la nave con sus diferentes estados.
-     *
      */
     private void initializeSpritesSubBuffer() {
         try {
-            this.spritesSubBuffer = ImageIO.read(getClass().getResource("../image/spritesArkanoid.png"));
+            this.spritesBuffer = ImageIO.read(getClass().getResource("/image/spritesArkanoid.png"));
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
 
+    /**
+     * Inicializa la imagen de la bola
+     *
+     */
     private void initializeBallBuffer() {
-        //Todo conectar todo, ya tengo la posicion y tamaños
+        this.ballSubBuffer = this.spritesBuffer.getSubimage(
+                POSITION_X_BALL,
+                POSITION_Y_BALL,
+                WIDTH_BALL_SPRITE,
+                HEIGHT_BALL_SPRITE);
     }
 
+    /**
+     * Inicializa la imagen de la fondo
+     *
+     */
     private void initializeBackgroundBuffer() {
-        //Todo
+        try {
+            this.backgroundBuffer = ImageIO.read(getClass().getResource("/image/Background.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
+    /**
+     * Inicializa la imagen de la nave
+     *
+     */
     private void initializeShipBuffer() {
-        //Todo conectar todo, ya tengo la posicion y tamaños
+        this.barShipSubBuffer = new BufferedImage[4];
+        for (int i = 0; i < this.barShipSubBuffer.length; i++) {
+            this.barShipSubBuffer[i] = this.spritesBuffer.getSubimage(
+                    POSITION_X_BAR_SHIP + ((SIZE_BETWEEN_BAR_SHIP + WIDTH_BAR_SHIP_SPRITE) * i),
+                    POSITION_Y_BAR_SHIP,
+                    WIDTH_BAR_SHIP_SPRITE,
+                    HEIGHT_BAR_SHIP_SPRITE
+            );
+        }
     }
 
+    /**
+     * Inicializa la imagen de la nave destruyendose
+     *
+     */
     private void initializeShipDestroidBuffer() {
-        //Todo
+        this.barShipDestroidSubBuffer = new BufferedImage[4];
+        this.barShipSubBuffer[0] = this.spritesBuffer.getSubimage(0, 44, 36, 13);
+        this.barShipSubBuffer[1] = this.spritesBuffer.getSubimage(41, 41, 30, 16);
+        this.barShipSubBuffer[2] = this.spritesBuffer.getSubimage(82, 41, 32, 16);
+        this.barShipSubBuffer[3] = this.spritesBuffer.getSubimage(123, 43, 30, 14);
+    }
+
+    /**
+     * Inicializa la imagen del icono de la vida
+     *
+     */
+    private void initializeLiveUpBuffer() {
+        this.liveUpSubBuffer = this.spritesBuffer.getSubimage(12, 14, 16, 7);
     }
 
     /**
      * Obtiene los Sprites correspondientes a los bloques, se obtiene mediante
      * subimagenes del sprite principal con posicionamiento y tamaño.
-     *
      */
     private void initializeBlockBuffer() {
         this.blockSubBuffer = new BufferedImage[10];
         for (int i = 0; i < this.blockSubBuffer.length; i++) {
-            this.blockSubBuffer[i] = this.spritesSubBuffer.getSubimage(
-                    POSITION_X_BLOCK *i/* Todo hay que solucionar que vaya cogiendo la siguiente posicion*/,
+            this.blockSubBuffer[i] = this.spritesBuffer.getSubimage(
+                    POSITION_X_BLOCK + (WIDTH_BLOCK * i),
                     POSITION_Y_BLOCK,
-                    WIDTH_BLOCK * i,
+                    WIDTH_BLOCK,
                     HEIGHT_BLOCK);
         }
     }
